c1ebb348d87a5ffd93e3fadde656d9e61a29bda3
Austin Spiegel <aspiegel93@gmail.com>
2014-02-23 22:24:44 -0800

    Working on the Vertical Slice

---
+++ ascent/Assets/Scripts/Climbing.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Climbing : MonoBehaviour {
+	
+	public float speed =1.0f;
+		
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(Keycode.UpArrow))
+		{
+			Vector3 newVelocity = transform.localScale;
+			newScale.y = 1.0f;
+
+			transform.Position = newVelocity * Time.deltaTime * speed;
+		} 
+		else if (Input.GetKey(Keycode.RightArrow))
+		{
+			Vector3 newVelocity = transform.localScale;
+			newScale.x = 1.0f;
+			
+			transform.Position = newVelocity * Time.deltaTime * speed;
+		} 
+		else if (Input.GetKey(Keycode.LeftArrow))
+		{
+			Vector3 newVelocity = transform.localScale;
+			newScale.x = -1.0f;
+			
+			transform.Position += newVelocity * Time.deltaTime * speed;
+		} 
+	}
+}
