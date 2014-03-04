6ecc52f3c47e128336c4dd5175f80162c0ae54b9
mgoulis <mgoulis@usc.edu>
2014-03-03 22:32:02 -0800

    new wurkz

---
+++ ascent/Assets/Scripts/C_Sharp/DontDieOnMeNow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDieOnMeNow : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision){
+		Player.transform.localPosition = new Vector3(8, -25, 0);
+	}
+
+}
