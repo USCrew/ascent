8a36e6543757fd7396e37dc8d08350f1e066807b
mgoulis <mgoulis@usc.edu>
2014-03-30 23:38:17 -0700

    IHATEMONDAYS

--- ascent/Assets/Scripts/C_Sharp/Climbing.cs
+++ ascent/Assets/Scripts/C_Sharp/Climbing.cs
@@ -21,16 +21,27 @@ public class Climbing : MonoBehaviour {
 	public bool isClimbing = false;
 	public bool isGrounded = false;
 
+	//Animation
+	public Animator anim;
+	
+
 	// Use this for initialization
 	void Start () {
 
 		setCurrentCheckpoint(StartingCheckpoint);
+		//anim.animation["Walk_Left"].speed = -1f;
+		//anim.animation["Walk_Right"].speed = 0.01f;
+		//anim.speed = 0.1f;
+
 
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+
+		//TODO THIS SHIT
+		/*
 
 		if (Input.GetKey(KeyCode.Escape))
 		{
@@ -41,9 +52,13 @@ public class Climbing : MonoBehaviour {
 			if( Time.timeScale > 0)
 			{
 				Time.timeScale =0;
+
 			}
+
 			else Time.timeScale = 1;
 		}
+
+		*/
 
 		if (Input.GetKeyDown("p")){
 			if( Time.timeScale > 0)
@@ -58,6 +73,31 @@ public class Climbing : MonoBehaviour {
 		if ((!Input.GetKey(KeyCode.UpArrow)&&!Input.GetKey(KeyCode.DownArrow)) && isClimbing){
 			rigidbody2D.velocity = Vector3.zero;
 		}
+
+
+			if(!isGrounded && !isClimbing)
+			{
+				anim.SetInteger("State", 0);
+				//anim.Play("Idle", 1, .01f);
+			}
+
+		if(isGrounded && rigidbody2D.velocity.x < 1 && !isClimbing)
+			{
+				anim.SetInteger("State", 0);
+				//anim.Play("Idle", 1, .01f);
+			}
+
+			if(isClimbing && Mathf.Abs(rigidbody2D.velocity.y)>1)
+			{
+				anim.SetInteger("State", 7);
+				//anim.Play("Climb_General", 1, .01f);
+			}
+
+			if(isClimbing && Mathf.Abs(rigidbody2D.velocity.y)<1)
+			{
+				anim.SetInteger("State", 0);
+				//anim.Play("Climb_General", 1, .01f);
+			}
 
 		/**
 		 *Code Used For Moving Up on Walls and Jumping
@@ -94,10 +134,13 @@ public class Climbing : MonoBehaviour {
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
 			rigidbody2D.AddForce(Vector3.right * horz_speed);
+				anim.SetInteger("State", 4);
+
 		} 
 		else if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			rigidbody2D.AddForce(Vector3.left * horz_speed);
+				anim.SetInteger("State", 1);
 			}
 		}
 
